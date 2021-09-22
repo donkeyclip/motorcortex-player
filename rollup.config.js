a0149b71ba359ea21c2995c88ad0710d79f05a13
Theofilos Papadopoulos <theofilos.p@kissmybutton.gr>
2021-09-22 12:45:55 +0300

    perf: bundle optimizations

--- rollup.config.js
+++ rollup.config.js
@@ -5,8 +5,14 @@ import html from "rollup-plugin-html";
 import { terser } from "rollup-plugin-terser";
 import postcss from "rollup-plugin-postcss";
 import svg from "rollup-plugin-svg";
-
+import svgo from "rollup-plugin-svgo";
 import pkg from "./package.json";
+import analyze from "rollup-plugin-analyzer";
+import cleanup from "rollup-plugin-cleanup";
+import visualizer from "rollup-plugin-visualizer";
+
+const ANALYZE = true;
+
 export default [
   {
     input: "src/index.js",
@@ -19,9 +25,21 @@ export default [
       resolve(),
       commonjs(),
       babel(),
-      postcss({ inject: false }),
-      html({ include: "**/*.html" }),
+      postcss({ inject: false, minimize: true }),
+      html({
+        include: "**/*.html",
+        htmlMinifierOptions: {
+          collapseWhitespace: true,
+          collapseBooleanAttributes: true,
+          conservativeCollapse: true,
+          minifyJS: true,
+        },
+      }),
+      svgo(),
       svg(),
+      visualizer(),
+      cleanup({ comments: "none" }),
+      ANALYZE && analyze(),
     ],
   },
   {
@@ -42,9 +60,21 @@ export default [
       commonjs(),
       babel(),
       terser(),
-      postcss({ inject: false }),
-      html({ include: "**/*.html" }),
+      postcss({ inject: false, minimize: true }),
+      html({
+        include: "**/*.html",
+        htmlMinifierOptions: {
+          collapseWhitespace: true,
+          collapseBooleanAttributes: true,
+          conservativeCollapse: true,
+          minifyJS: true,
+        },
+      }),
+      svgo(),
       svg(),
+      visualizer(),
+      cleanup({ comments: "none" }),
+      ANALYZE && analyze(),
     ],
   },
 ];
