e71312325584b992f5e1de24417316cc3dababab
alexdor <alexandros@dorodouli.com>
2021-10-21 13:31:45 +0000

    Deploying to gh-pages from @ donkeyclip/motorcortex-player@466c64a8bcc4f3a031cdc3bd6dc2c97f743411b9 🚀

--- rollup.config.js
+++ rollup.config.js
@@ -1,37 +1,38 @@
 import babel from "@rollup/plugin-babel";
 import commonjs from "@rollup/plugin-commonjs";
+import json from "@rollup/plugin-json";
 import resolve from "@rollup/plugin-node-resolve";
 import html from "rollup-plugin-html";
-import { terser } from "rollup-plugin-terser";
 import postcss from "rollup-plugin-postcss";
 import svg from "rollup-plugin-svg";
-
+import { terser } from "rollup-plugin-terser";
 import pkg from "./package.json";
+
 export default [
   {
     input: "src/index.js",
-    external: ["@kissmybutton/motorcortex"],
+    external: ["@donkeyclip/motorcortex"],
     output: [
       { file: pkg.main, format: "cjs" },
       { file: pkg.module, format: "es" },
     ],
     plugins: [
+      json(),
+      html({ fileName: "**/*.html" }),
+      svg(),
       resolve(),
       commonjs(),
       babel(),
-      terser(),
       postcss({ inject: false }),
-      html({ include: "**/*.html" }),
-      svg(),
     ],
   },
   {
     input: "src/index.js",
-    external: ["@kissmybutton/motorcortex"],
+    external: ["@donkeyclip/motorcortex"],
     output: [
       {
         globals: {
-          "@kissmybutton/motorcortex": "MotorCortex",
+          "@donkeyclip/motorcortex": "MotorCortex",
         },
         name: pkg.name,
         file: pkg.browser,
@@ -39,13 +40,14 @@ export default [
       },
     ],
     plugins: [
+      json(),
+      html({ include: "**/*.html" }),
       resolve({ mainFields: ["module", "main", "browser"] }),
       commonjs(),
       babel(),
-      terser(),
-      postcss({ inject: false }),
-      html({ include: "**/*.html" }),
       svg(),
+      postcss({ inject: false }),
+      terser(),
     ],
   },
 ];
