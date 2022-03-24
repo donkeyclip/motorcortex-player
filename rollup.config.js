f8ff232db5f838baf2b4f0ef0a59242a74a25f7b
Alexandros Dorodoulis <aldor94@gmail.com>
2022-03-24 12:58:39 +0200

    feat: compress css and html on bundle

--- rollup.config.js
+++ rollup.config.js
@@ -4,10 +4,14 @@ import json from "@rollup/plugin-json";
 import resolve from "@rollup/plugin-node-resolve";
 import html from "rollup-plugin-html";
 import postcss from "rollup-plugin-postcss";
+import svg from "rollup-plugin-svg";
 import cleanup from "rollup-plugin-cleanup";
 import { terser } from "rollup-plugin-terser";
 import pkg from "./package.json";
+
+import analyze from "rollup-plugin-analyzer";
 import svgo from "rollup-plugin-svgo";
+const ANALYZE = false;
 export default [
   {
     input: "src/index.js",
@@ -15,12 +19,24 @@ export default [
     output: [{ dir: pkg.module, format: "es" }],
     plugins: [
       json(),
+      html({
+        fileName: "**/*.html",
+        htmlMinifierOptions: {
+          collapseWhitespace: true,
+          collapseBooleanAttributes: true,
+          conservativeCollapse: true,
+          minifyJS: true,
+          removeComments: true,
+          collapseInlineTagWhitespace: true,
+          minifyCSS: true,
+        },
+      }),
       svgo({ multipass: true }),
-      html({ fileName: "**/*.html" }),
+      svg(),
       resolve(),
       commonjs(),
       babel(),
-      postcss({ inject: false }),
+      postcss({ inject: false, minimize: true }),
     ],
   },
   {
@@ -40,13 +56,26 @@ export default [
     plugins: [
       json(),
       svgo({ multipass: true }),
-      html({ include: "**/*.html" }),
+      svg(),
+      html({
+        include: "**/*.html",
+        htmlMinifierOptions: {
+          collapseWhitespace: true,
+          collapseBooleanAttributes: true,
+          conservativeCollapse: true,
+          minifyJS: true,
+          removeComments: true,
+          collapseInlineTagWhitespace: true,
+          minifyCSS: true,
+        },
+      }),
       resolve({ mainFields: ["module", "main", "browser"] }),
       commonjs(),
       babel(),
-      postcss({ inject: false }),
+      postcss({ inject: false, minimize: true }),
       cleanup({ comments: "none" }),
       terser(),
+      ANALYZE && analyze({ summaryOnly: true }),
     ],
   },
 ];
