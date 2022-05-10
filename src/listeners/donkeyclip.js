326864bf901fd1f99a1d697fafc554b4739f8cbe
Theofilos Papadopoulos <priestont@gmail.com>
2022-05-10 18:59:36 +0300

    fix: remove typo

--- src/listeners/donkeyclip.js
+++ src/listeners/donkeyclip.js
@@ -2,6 +2,6 @@ export default (_this) => {
   _this.elements.donkeyclipButton.addEventListener("click", () => {
     const id = window.location.pathname.split("/").pop();
     const staging = window.location.host.includes("staging") ? "staging." : "";
-    window.open(`https://${staging}donkeyclip.com/explore/donkeyclips/}${id}`);
+    window.open(`https://${staging}donkeyclip.com/explore/donkeyclips/${id}`);
   });
 };
