44e9bee54e7bf92e9ced62d95053d1dc26b88ab2
Theofilos Papadopoulos <priestont@gmail.com>
2022-10-21 18:58:03 +0300

    fix: mc themes and loopbar

--- src/listeners/donkeyclip.js
+++ src/listeners/donkeyclip.js
@@ -1,7 +1,9 @@
 export default (_this) => {
   _this.elements.donkeyclipButton.addEventListener("click", () => {
-    const id = window.location.pathname.split("/").pop();
+    const clipID = window.DonkeyClip.clipId;
     const staging = window.location.host.includes("staging") ? "staging." : "";
-    window.open(`https://${staging}donkeyclip.com/explore/donkeyclips/${id}`);
+    window.open(
+      `https://${staging}donkeyclip.com/explore/donkeyclips/${clipID || ""}`
+    );
   });
 };
