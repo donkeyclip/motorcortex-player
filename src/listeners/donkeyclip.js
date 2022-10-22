33e41508db5fa201a4be8602663ba089697b5481
Alexandros Dorodoulis <aldor94@gmail.com>
2022-10-22 11:27:53 +0200

    fix: open dc's home if no clip id

--- src/listeners/donkeyclip.js
+++ src/listeners/donkeyclip.js
@@ -1,9 +1,9 @@
 export default (_this) => {
   _this.elements.donkeyclipButton.addEventListener("click", () => {
-    const clipID = window.DonkeyClip.clipId;
+    const clipID = window.DonkeyClip?.clipId;
     const staging = window.location.host.includes("staging") ? "staging." : "";
     window.open(
-      `https://${staging}donkeyclip.com/explore/donkeyclips/${clipID || ""}`
+      `https://${staging}donkeyclip.com/${clipID ? `explore/donkeyclips/${clipID}` : ""}`
     );
   });
 };
